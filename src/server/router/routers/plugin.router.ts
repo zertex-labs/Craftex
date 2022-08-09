5298e2ea63cb9415a6530a07d7849265edd320c6
xrexy <rexydoesimportantthings@gmail.com>
2022-08-09 18:42:01 +0300

    Some trpc router stuff and forms

--- src/server/router/routers/plugin.router.ts
+++ src/server/router/routers/plugin.router.ts
@@ -1,21 +1,32 @@
-import { PluginModel, RelatedPluginModel } from "../../../../prisma/models";
+import { z } from "zod";
 import { createRouter } from "../context";
-import { createProtectedRouter } from "../protected-router";
 
-export const protectedPluginRouter = createProtectedRouter().mutation(
-  "create",
-  {
-    input: PluginModel,
-    resolve: ({ input, ctx: { prisma: db } }) =>
-      db.plugin.create({
-        data: input,
-        select: {
-          id: true,
-        },
-      }),
-  }
-);
-
-export const unprotectedPluginRouter = createRouter().query("all", {
-  resolve: ({ ctx: { prisma: db } }) => db.plugin.findMany(),
+export const PluginDto = z.object({
+  title: z.string(),
 });
+
+const protectedPluginRouter = createRouter().mutation("create", {
+  input: PluginDto,
+  resolve: ({ input, ctx: { prisma: db } }) =>
+    db.plugin.create({
+      data: input,
+      select: {
+        id: true,
+      },
+    }),
+});
+
+const unprotectedPluginRouter = createRouter().query("all", {
+  resolve: ({ ctx: { prisma: db } }) =>
+    db.plugin.findMany({
+      select: {
+        id: true,
+        title: true,
+        developers: true,
+      },
+    }),
+});
+
+export const pluginRouter = createRouter()
+  .merge(protectedPluginRouter)
+  .merge("unprotected.", unprotectedPluginRouter);
---
+++ src/server/router/routers/user.router.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+import { createProtectedRouter } from "../protected-router";
+
+export const UserDto = z.object({
+  name: z.string(),
+  email: z.string().email(),
+});
+
+export const userRouter = createProtectedRouter().query("all", {
+  resolve: ({ ctx: { prisma: db } }) => db.user.findMany(),
+});
