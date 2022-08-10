1f734a6436d369b1102aa24c19d046f7205b0849
xrexy <rexydoesimportantthings@gmail.com>
2022-08-10 18:55:34 +0300

    Add controls to root page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 
 const Home: NextPage = () => {
+  const { data: session, status } = useSession();
+
   return (
     <>
       <Head>
@@ -9,9 +12,20 @@ const Home: NextPage = () => {
         <meta name="description" content="Craftex TOOD" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <h1>Home</h1>
-      </div>
+
+      {status === "authenticated" && session.user ? (
+        <div>
+          <h1>
+            Welcome {session.user.name} ({session.user.email})
+          </h1>
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
+      ) : (
+        <div>
+          <h1>Bruh, sign in??</h1>
+          <button onClick={() => signIn()}>Sign in</button>
+        </div>
+      )}
     </>
   );
 };
