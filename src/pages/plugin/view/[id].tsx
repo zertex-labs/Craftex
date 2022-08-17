4b2556312df23600aca817862fb295bcc0e00863
xrexy <rexydoesimportantthings@gmail.com>
2022-08-17 23:56:11 +0300

    Move plugin view to view folder && redirect on failed fetch

--- src/pages/plugin/view/[id].tsx
+++ src/pages/plugin/view/[id].tsx
@@ -1,7 +1,8 @@
 import { createSSG } from "@server/router";
 import { trpc } from "@utils/trpc";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useReducer } from "react";
 
 export async function getServerSideProps(
   context: GetServerSidePropsContext<{ id: string }>
@@ -9,17 +10,10 @@ export async function getServerSideProps(
   const ssg = await createSSG();
   const id = context.params?.id as string;
 
-  console.log(id);
-
-  /*
-   * Prefetching the `post.byId` query here.
-   * `prefetchQuery` does not return the result - if you need that, use `fetchQuery` instead.
-   */
   await ssg.prefetchQuery("plugin.unprotected.byId", {
     id,
   });
 
-  // Make sure to return { props: { trpcState: ssg.dehydrate() } }
   return {
     props: {
       trpcState: ssg.dehydrate(),
@@ -32,21 +26,25 @@ export default function PluginView(
   props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) {
   const { id } = props;
+  const { push: redirect } = useRouter();
 
-  // This query will be immediately available as it's prefetched.
-  const { data: plugin, isFetching } = trpc.useQuery([
-    "plugin.unprotected.byId",
-    { id },
-  ]);
+  const { data: plugin, isFetching } = trpc.useQuery(
+    ["plugin.unprotected.byId", { id }],
+    { onError: () => redirect("/"), refetchOnWindowFocus: false }
+  );
 
   return (
     <React.Fragment>
       {isFetching && !plugin && <h1>Loading...</h1>}
 
-      <h1>{plugin?.title}</h1>
+      {plugin && (
+        <React.Fragment>
+          <h1>{plugin.title}</h1>
 
-      <h2>Raw data:</h2>
-      <pre>{JSON.stringify(plugin, null, 2)}</pre>
+          <h2>Raw data:</h2>
+          <pre>{JSON.stringify(plugin, null, 2)}</pre>
+        </React.Fragment>
+      )}
     </React.Fragment>
   );
 }
