cf39748bc1be049e43623bf8fb11449a6a8dc54f
xrexy <rexydoesimportantthings@gmail.com>
2022-08-18 00:13:09 +0300

    Set "all" plugins query stale time to one minute

--- src/pages/plugin/all.tsx
+++ src/pages/plugin/all.tsx
@@ -3,25 +3,25 @@ import { trpc } from "@utils/trpc";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import React from "react";
 
-export async function getServerSideProps(
-  context: GetServerSidePropsContext<{ id: string }>
-) {
-  const ssg = await createSSG();
-  await ssg.prefetchQuery("plugin.unprotected.all");
+// export async function getServerSideProps(
+//   context: GetServerSidePropsContext<{ id: string }>
+// ) {
+//   const ssg = await createSSG();
+//   await ssg.prefetchQuery("plugin.unprotected.all");
 
-  return {
-    props: {
-      trpcState: ssg.dehydrate(),
-    },
-  };
-}
+//   return {
+//     props: {
+//       trpcState: ssg.dehydrate(),
+//     },
+//   };
+// }
 
-export default function PluginView(
-  props: InferGetServerSidePropsType<typeof getServerSideProps>
-) {
-  const { data: plugins, isFetching } = trpc.useQuery([
-    "plugin.unprotected.all",
-  ]);
+export default function PluginView() {
+  // props: InferGetServerSidePropsType<typeof getServerSideProps>
+  const { data: plugins, isFetching } = trpc.useQuery(
+    ["plugin.unprotected.all"],
+    { staleTime: 60 * 1000 }
+  );
 
   return (
     <React.Fragment>
