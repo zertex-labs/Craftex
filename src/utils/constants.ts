c3837cf706286f963789148da37d670636db378b
xrexy <rexydoesimportantthings@gmail.com>
2022-08-21 16:57:29 +0300

    VERY Temporary create page

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,8 @@
+import { MIME_TYPES } from "@mantine/dropzone";
+
 export const ERROR_LENGTH_MS = 5000;
 
-export const SUPPORTED_FORMATS = ["image/jpeg", "image/png", "image/webp"];
-export const MAX_FILE_SIZE = 5 * (10 ** 6 * 8);
+export const SUPPORTED_FORMATS = [MIME_TYPES.png, MIME_TYPES.jpeg];
+export const MAX_FILE_SIZE = 5 * (10 ** 6);
 
-export const CDN_URL = "https://cdn.craftex.dev/plugins/"
+export const CDN_URL = "https://cdn.craftex.dev/plugins/";
