4a6dc20e64dbfc726865839cb5c9939712ff5c46
xrexy <rexydoesthings@gmail.com>
2023-04-25 00:26:32 +0300

    remove unused endpoints

--- api/src/routes/plugin.ts
+++ api/src/routes/plugin.ts
@@ -2,13 +2,4 @@ import { Router } from "oak/mod.ts";
 
 const pluginRouter = new Router({});
 
-pluginRouter.get("/", (ctx) => {
-  ctx.response.body = "Hello from plugin";
-});
-
-// with param
-pluginRouter.get("/:id/:id2", (ctx) => {
-  ctx.response.body = `Hello from plugin with id ${ctx.params.id}`;
-});
-
 export default pluginRouter;
