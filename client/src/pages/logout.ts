a72bc0333973a33d1c59d1c06d187958f4ede08b
xrexy <rexydoesthings@gmail.com>
2023-09-18 14:08:59 +0300

    logout

---
+++ client/src/pages/logout.ts
@@ -0,0 +1,23 @@
+import type { APIRoute } from "astro";
+import { auth } from "../lib/lucia";
+
+export const post: APIRoute = async ({ locals, redirect }) => {
+  const session = await locals.auth.validate();
+  if (!session) {
+    return new Response(
+      JSON.stringify({
+        error: "Unauthorized",
+      }),
+      {
+        status: 401,
+      }
+    );
+  }
+
+  await auth.invalidateSession(session.sessionId);
+
+  locals.auth.setSession(null);
+
+  return redirect("/login", 302);
+};
+
